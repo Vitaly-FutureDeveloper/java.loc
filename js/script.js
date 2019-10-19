75abde57e8f02de5d7d5078ccd7ea1cf5e0dd14e
Develop JS lesson <zarvoron@yandex.ru>
2019-10-19 18:03:30 +0300

    Made Timer

--- js/script.js
+++ js/script.js
@@ -37,6 +37,70 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    
+    /*
+    Timer
+    */
+
+   let deadline = "2019-10-20"; //Конечная дата
+
+   /*Высчитывает разницу времени от конечной даты до сегодняшней
+   вернёт объект Конечная дата, часы, минуты, сек.*/
+       function getTimeRemaning(endtime){
+           let t = Date.parse(endtime) - Date.parse(new Date()),
+               seconds = Math.floor((t/1000) % 60),
+               minutes = Math.floor((t/1000/60) % 60),
+               hours = Math.floor((t/(1000*60*60)));
+   
+           return {
+               'total' : t,
+               'hours' : hours,
+               'minutes' : minutes,
+               'seconds' : seconds
+           };
+       }
+   
+       function setClock(id, endtime) {
+           let timer = document.getElementById(id),
+               hours = timer.querySelector('.hours'),
+               minutes = timer.querySelector('.minutes'),
+               seconds = timer.querySelector('.seconds'),
+   /*после захвата строк в вёрстке - запускаем функцию updateClock() каждую 1сек*/
+               timeInterval = setInterval(updateClock, 1000);
+   
+           function updateClock() //Подставляет значения в поля DOM по интервалу (выше)
+           {
+   //Запишем в переменную t объект "разница времени"
+               let t = getTimeRemaning(endtime);
+   
+   /*группа проверок на кол-во цифр, чтоб не было по 1, а было 01*/
+                   if(t.hours.toString().length == '1') {
+                       t.hours = "0" + t.hours;
+                   }
+                   if(t.minutes.toString().length == '1') {
+                       t.minutes = "0" + t.minutes;
+                   }
+                   if(t.seconds.toString().length == '1') {
+                       t.seconds = "0" + t.seconds;
+                   }
+   /*Пишем в DOM обекты таймера: часы, минуты, сек*/
+               hours.textContent = t.hours;
+               minutes.textContent = t.minutes;
+               seconds.textContent = t.seconds;
+   
+   /*Приверка на "время закончилось", прерывание интервала*/
+               if(t.total <= 0 ) {
+                   clearInterval(timeInterval);
+   /*Пишем в DOM обекты таймера: часы, минуты, сек - время по нулям*/
+                       hours.textContent = '00';
+                       minutes.textContent = '00';
+                       seconds.textContent = '00';
+               }
+           }
+       }
+   
+       setClock('timer', deadline);
+
 });
 
 
